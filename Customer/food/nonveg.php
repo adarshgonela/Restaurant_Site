55f8ae404375ad6ce4768b060bf2a518ead8ae3b
Unkn0wnbrain <harshavardhan9932@gmail.com>
2024-05-21 13:29:31 +0530

    added food

--- Customer/food/nonveg.php
+++ Customer/food/nonveg.php
@@ -4,11 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php include('header.php')?>
 </head>
 <body>
-    <?php include('sidebar.php')?>
     <?php include('navbar.php')?>
-    <?php include('footer.php')?>
+    <?php include('sidebar.php')?>
 </body>
 </html>
