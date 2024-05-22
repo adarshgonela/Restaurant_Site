1e473ac15ad4334b50807af85cb87d08382e8f71
Chandram1112 <gchandram117@gmail.com>
2024-05-22 13:27:43 +0530

    sweets page done

--- Customer/sql/sweetsSql.php
+++ Customer/sql/sweetsSql.php
@@ -7,9 +7,9 @@ function getSweets()
     $result= mysqli_query($conn,$sql);
     $sweets=[];
     while($row=mysqli_fetch_assoc($result)){
-        $sweets=$row;
+        $sweets[]=$row;
     }
-    
+
     return $sweets;
 }
 ?>
