7569f6e4ab9bc3a813073786236dd5f50663f069
Unkn0wnbrain <harshavardhan9932@gmail.com>
2024-05-27 17:44:55 +0530

    added links

--- Customer/redirect/login.php
+++ Customer/redirect/login.php
@@ -17,7 +17,7 @@
   <!-- Right Side (Form Section) -->
   <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
     <div class="w-full h-100">
-      <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
+      <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12 ">Log in to your account</h1>
       
       <form class="mt-6" action="#" method="POST">
         <div>
@@ -46,7 +46,7 @@
         </div>
       </button>
 
-      <p class="mt-8">Need an account? <a href="#" class="text-blue-500 hover:text-blue-700 font-semibold">Create an account</a></p>
+      <p class="mt-8">Need an account? <a href="http://localhost/Github/Restaurant_Site/Customer/redirect/signup.php#" class="text-blue-500 hover:text-blue-700 font-semibold">Create an account</a></p>
     </div>
   </div>
 
--- Customer/redirect/signup.php
+++ Customer/redirect/signup.php
@@ -18,7 +18,7 @@
   <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
     <div class="w-full">
       <p class="text-4xl font-medium text-center leading-snug font-serif">Sign up for an account</p>
-      <form class="w-full mt-6 space-y-8" action="#" method="POST">
+      <form class="w-full mt-10 space-y-8" action="#" method="POST">
         <div class="relative">
           <label for="username" class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 font-medium text-gray-600 absolute">Username</label>
           <input id="username" name="username" placeholder="John" type="text" class="border placeholder-gray-400 focus:outline-none focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 text-base block bg-white border-gray-300 rounded-md"/>
@@ -32,7 +32,7 @@
           <input id="password" name="password" placeholder="Password" type="password" class="border placeholder-gray-400 focus:outline-none focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 text-base block bg-white border-gray-300 rounded-md"/>
         </div>
         <div class="text-right mt-2">
-        <p class="mt-8 text-center">Already have an account? <a href="#" class="text-blue-500 hover:text-blue-700 font-semibold">Log in</a></p>
+        <p class="mt-8 text-center">Already have an account? <a href="http://localhost/Github/Restaurant_Site/Customer/redirect/login.php#" class="text-blue-500 hover:text-blue-700 font-semibold">Log in</a></p>
         </div>
         <hr class="my-6 border-gray-300 w-full">
         <div class="relative">
