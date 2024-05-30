dcbd22bab00bb65c34a113c36ccf3bd8c8529360
Unkn0wnbrain <harshavardhan9932@gmail.com>
2024-05-30 10:42:59 +0530

    changed login

--- Customer/redirect/login.php
+++ Customer/redirect/login.php
@@ -16,18 +16,16 @@
 
   <!-- Right Side (Form Section) -->
   <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
-    <div class="w-full h-100">
-      <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12 ">Log in to your account</h1>
-      
-      <form class="mt-6" action="#" method="POST">
-        <div>
-          <label for="email" class="block text-gray-700 font-bold ">Email Address:</label>  
-          <input type="email" id="email" name="email" placeholder="Enter Email Address" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete="email" required>
+    <div class="w-full">
+      <p class="text-4xl font-medium text-center leading-snug font-serif">Login into your  account</p>
+      <form class="w-full mt-10 space-y-8" action="#" method="POST">
+        <div class="relative">
+          <label for="email" class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 font-medium text-gray-600 absolute">Email address:</label>
+          <input id="email" name="email" placeholder="Enter your email" type="email" class="border placeholder-gray-400 focus:outline-none focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 text-base block bg-white border-gray-300 rounded-md autofocus autocomplete="email" required>
         </div>
-
-        <div class="mt-4">
-          <label for="password" class="block text-gray-700 font-bold">Password:</label>
-          <input type="password" id="password" name="password" placeholder="Enter Password" minlength="6" class="border placeholder-gray-400 focus:outline-none focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 text-base block bg-white border-gray-300 rounded-md"/>
+        <div class="relative">
+          <label for="password" class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 font-medium text-gray-600 absolute">Password:</label>
+          <input id="password" name="password" placeholder=" Enter Password" type="password" class="border placeholder-gray-400 focus:outline-none focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 text-base block bg-white border-gray-300 rounded-md autofocus autocomplete="password" required>
         </div>
 
         <div class="text-right mt-2">
--- Customer/redirect/signup.php
+++ Customer/redirect/signup.php
@@ -10,7 +10,7 @@
 <section class="flex flex-col md:flex-row h-screen items-center">
 
   <!-- Left Side (Image Section) -->
-  <div class="bg-indigo-600 w-full md:w-1/2 xl:w-2/3 ">
+  <div class="bg-indigo-600 w-full md:w-1/2 xl:w-2/3 h-screen">
     <img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Random image from Unsplash" class="w-full h-full object-cover">
   </div>
 
